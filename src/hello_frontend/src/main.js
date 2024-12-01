c8c846db12be7e1dec632793c0668e759cfb1ff0
kia <sheikhykia@gmail.com>
2024-12-01 10:23:13 +0330

    modifying the js files

--- src/hello_frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { hello, hello_backend } from "../../declarations/hello_backend/hello_backend.did";
-
-document.addEventListener('load', () => {
-    let title = document.getElementById('add');
-    title.addEventListener('click', () => {
-        let a = hello_backend.Add(5, 10);
-        console.log('it works');
-    });
-})
